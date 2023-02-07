5a202237250252587552cd0de05db2223263a04e
ceb <ceb5121@gmail.com>
2023-02-07 23:01:31 +0900

    Initial commit

---
+++ src/test/java/com/human/ex/LearnSpringBoot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.human.ex.LearnSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
