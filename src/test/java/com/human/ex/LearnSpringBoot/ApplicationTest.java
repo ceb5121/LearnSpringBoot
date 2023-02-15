bdf80c469cdb3657bbc5d425647c9735cbc82330
ceb <ceb5121@gmail.com>
2023-02-15 23:02:57 +0900

    테스트케이스 작성. 참고하는 책 초판 인쇄가 2019년이라 버전도 안 맞고 현시점에서 지원 종료된 걸 많이 써서 간단한 테스트케이스도 오류난다

--- src/test/java/com/human/ex/LearnSpringBoot/ApplicationTest.java
+++ src/test/java/com/human/ex/LearnSpringBoot/ApplicationTest.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnSpringBootApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringBootApplication.class, args);
-	}
-
+public class ApplicationTest {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationTest.class, args);
+    }
 }
